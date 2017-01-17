7df57fca105656f7900b8992d13c24fd00ad67d1
thammaneni <thammaneni54@gmail.com>
2017-01-17 10:37:00 +0530

    adding Helloworld Directive to index.html and app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-angular.module("ngClassifieds",[]);
+angular
+	.module("ngClassifieds",["ngMaterial"])
+	.config(function($mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('teal')
+			.accentPalette('orange');
+
+	})
+	.directive("helloWorld", function() {
+		return {
+			template: "<h1> Hello, World!! <br> I am Srinivas </h1>"
+		}
+	});
