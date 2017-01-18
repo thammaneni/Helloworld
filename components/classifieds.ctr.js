1801783d2e79bff917b162c5dcdee118c1423c52
thammaneni <thammaneni54@gmail.com>
2017-01-18 22:20:58 +0530

    adding sidenav bar to window and make a form in sidebar

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,12 +3,21 @@
 
 	angular
 	  .module("ngClassifieds")
-	  .controller("classCtrl", function($scope, $http,classifiedFactory) {
+	  .controller("classCtrl", function($scope, $http,classifiedFactory,$mdSidenav) {
 
 	  	classifiedFactory.getClassifieds().then(function(classifieds) {
 	  		// body...
 	  		$scope.classifieds=classifieds.data;
-	  	})
+	  	});
+
+	  	$scope.openSidebar = function(){
+	  		$mdSidenav('left').open();
+	  	};
+	  	$scope.closeSidebar = function(){
+	  		$mdSidenav('left').close();
+	  	}
+
+
   
 
   });
